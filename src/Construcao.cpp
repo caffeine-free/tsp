7c63a193b65195c728422744c17de032fbabfbe1
Gabriel Araújo <gabrielcaetanodm@gmail.com>
2022-08-15 14:50:07 -0300

    task: greed constructive and best improvement heuristic

--- src/Construcao.cpp
+++ src/Construcao.cpp
@@ -10,38 +10,58 @@
 
 using namespace std;
 
-void constroi_solucao(int n, vector<int>& s, float **distancia)
+void constroi_solucao(int n, vector<int> &s, float **distancia)
 {
     s.clear(); // limpa solucao
-    //insere todas cidades sequencialmente
-    for (int j=0; j < n; j++) s.push_back(j);
+    // insere todas cidades sequencialmente
+    for (int j = 0; j < n; j++)
+        s.push_back(j);
 }
 
 /* Constroi uma solucao de forma gulosa, no caso,
    implementa o Metodo construtivo do vizinho mais proximo */
-void constroi_solucao_gulosa_vizinho_mais_proximo(int n, vector<int> &s, float **d)
-{
-  vector<int> nao_visitada; //lista das cidades ainda nao visitadas
+void constroi_solucao_gulosa_vizinho_mais_proximo(
+    int n, 
+    vector<int> &s, 
+    float **d
+) {
+    vector<int> nao_visitada; // lista das cidades ainda nao visitadas
 
-  // inserir um elemento no final de uma lista
-  for(int i = 1; i < n; i++)
-    nao_visitada.push_back(i);
+    // inserir um elemento no final de uma lista
+    for (int i = 1; i < n; i++)
+        nao_visitada.push_back(i);
 
-  s.clear(); // limpa solucao
-  s.push_back(0); // A cidade de origem é a cidade 0
+    s.clear();      // limpa solucao
+    s.push_back(0); // A cidade de origem é a cidade 0
 
-  int mais_proxima; // armazena a cidade mais proxima para inserir na solucao
-  float dist; // armazena a menor distancia
+    int origem;       // armazena a cidade de origem atual
+    int mais_proxima; // armazena a cidade mais proxima para inserir na solucao
+    float dist;       // armazena a menor distancia
+    
+    while (!nao_visitada.empty()) {
+        dist = INT_MAX;
+        origem = s.back();
 
-/*
-	Implementar o loop do metodo construtivo, 
-	inserindo sempre a cidade mais proxima ainda
-	nao visitada na solucao.
-*/
-	
+        // encontra a cidade mais próxima entre as cidades não visitadas
+        for (int destino : nao_visitada) {
+            if (d[origem][destino] < dist) {
+                dist = d[origem][destino];
+                mais_proxima = destino;
+            }
+        }
+
+        // adiciona a cidade mais próxima à solução e a remove da lista
+        s.push_back(mais_proxima);
+        nao_visitada.erase(
+            remove(
+                nao_visitada.begin(), 
+                nao_visitada.end(), 
+                mais_proxima
+            ), 
+            nao_visitada.end()
+        );
+    }
 }
-
-
 
 /* Constroi uma solucao de forma aleatoria */
 void constroi_solucao_aleatoria(int n, vector<int> &s, float **d)
@@ -49,14 +69,13 @@ void constroi_solucao_aleatoria(int n, vector<int> &s, float **d)
 
     constroi_solucao(n, s, d);
 
-    //Para c++ 11
+    // Para c++ 11
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     default_random_engine rand_seed(seed);
-    shuffle ( s.begin(), s.end(), rand_seed );
+    shuffle(s.begin(), s.end(), rand_seed);
 
-    //Para c++ 98
-    //random_shuffle ( s.begin(), s.end() );
-
+    // Para c++ 98
+    // random_shuffle ( s.begin(), s.end() );
 }
 
 /* Constroi uma solucao pela insercao mais barata */
@@ -67,14 +86,14 @@ void constroi_solucao_gulosa_insercao_mais_barata(int n, vector<int> *s, float *
     /* Inicio da Fase de Construcao de uma solucao */
     cout << "\n Construindo nova solucao ...\n";
 
-    for (int i=1; i<n; i++){
+    for (int i = 1; i < n; i++)
+    {
 
         /* vou inserir um registro no final de uma lista  */
         nao_visitada.push_back(i);
-
     }
     s->clear();
-    s->push_back(0);  /* A cidade origem é a cidade 0 */
+    s->push_back(0); /* A cidade origem é a cidade 0 */
 
     int j = 1;
     int mais_proxima;
@@ -84,17 +103,19 @@ void constroi_solucao_gulosa_insercao_mais_barata(int n, vector<int> *s, float *
     int melhor_i, melhor_k;
     float sij, melhor_sij;
 
-
     /* Insere as duas proximas cidades pela heuristica do vizinho mais proximo */
 
-    while ( j < 3){
+    while (j < 3)
+    {
         dist = INT_MAX;
         int i = 0;
         int pos_i = 0;
 
-        while (i < nao_visitada.size()) {
-            if (d[s->at(j-1)][nao_visitada[i]] < dist){
-                dist = d[s->at(j-1)][nao_visitada[i]];
+        while (i < nao_visitada.size())
+        {
+            if (d[s->at(j - 1)][nao_visitada[i]] < dist)
+            {
+                dist = d[s->at(j - 1)][nao_visitada[i]];
                 mais_proxima = nao_visitada[i];
                 pos_i = i;
             }
@@ -108,49 +129,54 @@ void constroi_solucao_gulosa_insercao_mais_barata(int n, vector<int> *s, float *
         j++;
     }
 
+    /* Formada uma subrota inicial com tres cidades, aplicar o metodo da
+       insercao mais barata para decidir qual cidade inserir entre cada
+       par de cidades i e j. A cidade k escolhida sera aquela que minimizar
+       custo(k) = d(i,k) + d(k,j) - d(i,j) */
 
-   /* Formada uma subrota inicial com tres cidades, aplicar o metodo da
-      insercao mais barata para decidir qual cidade inserir entre cada
-      par de cidades i e j. A cidade k escolhida sera aquela que minimizar
-      custo(k) = d(i,k) + d(k,j) - d(i,j) */
+    while (j < n)
+    {
+        melhor_sij = INT_MAX;
 
-    while (j < n) {
-      melhor_sij = INT_MAX;
+        int k = 0;
+        int pos_k;
+        /* Calcula os custos para cada cidade k ser inserida entre as cidades i e j */
+        while (k < nao_visitada.size())
+        {
+            cid_k = nao_visitada[k];
+            for (int i = 0; i < j; i++)
+            {
+                cid_i = s->at(i);
+                if ((i + 1) != j)
+                    cid_j = s->at(i + 1);
+                else
+                    cid_j = 0;
 
-      int k = 0;
-      int pos_k;
-      /* Calcula os custos para cada cidade k ser inserida entre as cidades i e j */
-      while (k < nao_visitada.size()) {
-        cid_k = nao_visitada[k];
-        for (int i = 0; i < j; i++) {
-	      cid_i = s->at(i);
-          if ((i+1) != j) cid_j = s->at(i+1);
-          else cid_j = 0;
+                sij = d[cid_i][cid_k] + d[cid_k][cid_j] - d[cid_i][cid_j];
 
-          sij = d[cid_i][cid_k] + d[cid_k][cid_j] - d[cid_i][cid_j];
-
-          if (sij < melhor_sij) {
- 	        melhor_i = cid_i;
-            //melhor_j = cid_j;
-            melhor_k = cid_k;
-            melhor_sij = sij;
-            pos = i + 1;
-            pos_k = k;
-          }
+                if (sij < melhor_sij)
+                {
+                    melhor_i = cid_i;
+                    // melhor_j = cid_j;
+                    melhor_k = cid_k;
+                    melhor_sij = sij;
+                    pos = i + 1;
+                    pos_k = k;
+                }
+            }
+            k++;
         }
-        k++;
-      }
 
-      /* Adiciona cada nova cidade k entre as cidades i e j que produzir a insercao mais barata */
-      //insere_meio_vetor(s,melhor_k,pos,j);
-      s->insert(s->begin()+pos,melhor_k);
+        /* Adiciona cada nova cidade k entre as cidades i e j que produzir a insercao mais barata */
+        // insere_meio_vetor(s,melhor_k,pos,j);
+        s->insert(s->begin() + pos, melhor_k);
 
-      //imprime_rota(s,n);
+        // imprime_rota(s,n);
 
-      /* Apaga a cidade mais barata da lista de nao visitadas */
+        /* Apaga a cidade mais barata da lista de nao visitadas */
         nao_visitada.erase(nao_visitada.begin() + pos_k);
 
-      j++;
+        j++;
     }
 }
 
@@ -158,23 +184,22 @@ void constroi_solucao_gulosa_insercao_mais_barata(int n, vector<int> *s, float *
 void constroi_solucao_parcialmente_gulosa_vizinho_mais_proximo(int n, vector<int> &s, float **d, float alpha)
 {
 
-    vector<int> nao_visitadas; 
+    vector<int> nao_visitadas;
     int tamanho_LC;
 
     /* Inicio da Fase de Construcao de uma solucao */
-    for (int i=1; i<n; i++){
+    for (int i = 1; i < n; i++)
+    {
 
         /* vou inserir um registro no final de uma lista das cidades nao visitadas */
         nao_visitadas.push_back(i);
-
     }
 
-    //limpa solucao corrente
+    // limpa solucao corrente
     s.clear();
-    s.push_back(0);  /* A cidade origem � a cidade 0 */
+    s.push_back(0); /* A cidade origem � a cidade 0 */
 
-    
-    //Ordena lista
+    // Ordena lista
     ordena_dist_crescente ordem;
     ordem.d = d; // fornece a matriz de distancia para usar na ordenacao
     ordem.index = s[0];
@@ -182,21 +207,20 @@ void constroi_solucao_parcialmente_gulosa_vizinho_mais_proximo(int n, vector<int
     /* Ordenando a lista de cidade nao visitadas */
     stable_sort(nao_visitadas.begin(), nao_visitadas.end(), ordem);
 
-
     int j = 1;
     int cidade_escolhida;
-    while (j < n){
+    while (j < n)
+    {
 
-      /*
-      *
-      *   Implementar construção da solução elemento por elemento
-      *
-      */
+        /*
+         *
+         *   Implementar construção da solução elemento por elemento
+         *
+         */
     }
 }
 
 /* Constroi uma solucao parcialmente gulosa com base no metodo da insercao mais barata */
 void constroi_solucao_parcialmente_gulosa_insercao_mais_barata(int n, vector<int> &s, float **d, float alpha)
 {
-
 }
--- src/Descida.cpp
+++ src/Descida.cpp
@@ -14,166 +14,174 @@ using namespace std;
 
 float calcula_delta(int n, vector<int> &s, float **d, int i, int j)
 {
-  int i_antes, i_depois, j_antes, j_depois;
+    int i_antes, i_depois, j_antes, j_depois;
 
-  i_antes = i - 1;
-  i_depois = i + 1;
-  j_antes = j - 1;
-  j_depois = j + 1;
-  if (i == 0) i_antes = n-1;
-  if (i == n-1) i_depois = 0;
-  if (j == 0) j_antes = n-1;
-  if (j == n-1) j_depois = 0;
+    i_antes = i - 1;
+    i_depois = i + 1;
+    j_antes = j - 1;
+    j_depois = j + 1;
+    if (i == 0)
+        i_antes = n - 1;
+    if (i == n - 1)
+        i_depois = 0;
+    if (j == 0)
+        j_antes = n - 1;
+    if (j == n - 1)
+        j_depois = 0;
 
-  float delta = d[s[i_antes]][s[i]] + d[s[i]][s[i_depois]] + 
-                d[s[j_antes]][s[j]] + d[s[j]][s[j_depois]];
-  return  delta;
-
-
+    float delta = d[s[i_antes]][s[i]] + d[s[i]][s[i_depois]] +
+                  d[s[j_antes]][s[j]] + d[s[j]][s[j_depois]];
+    return delta;
 }
 
 // Encontra o melhor vizinho utilizando o movimento de troca de posicao de duas cidades
 float melhor_vizinho(int n, vector<int> &s, float **d, float fo, int *melhor_i, int *melhor_j)
 {
-  float fo_melhor_viz = fo;
+    float fo_melhor_viz = fo;
 
-  float fo_viz;
-  for(int i=0 ; i < n - 1 ; i++){
-    for(int j=i+1 ; j < n ; j++) {
-      // Calcula a variacao de custo com a realizacao do movimento
-      float delta1 = calcula_delta(n,s,d,i,j);
+    float fo_viz;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // Calcula a variacao de custo com a realizacao do movimento
+            float delta1 = calcula_delta(n, s, d, i, j);
 
-      // Faz o movimento
-      swap(s[i], s[j]);
+            // Faz o movimento
+            swap(s[i], s[j]);
 
-      float delta2 = calcula_delta(n,s,d,i,j);
+            float delta2 = calcula_delta(n, s, d, i, j);
 
-      // Calcular a nova distancia
-      fo_viz = fo - delta1 + delta2;
+            // Calcular a nova distancia
+            fo_viz = fo - delta1 + delta2;
 
-      // Armazenar o melhor movimento (melhor troca)
-      if(fo_viz < fo_melhor_viz){
-        *melhor_i = i;
-        *melhor_j = j;
-        fo_melhor_viz = fo_viz;
-      }
+            // Armazenar o melhor movimento (melhor troca)
+            if (fo_viz < fo_melhor_viz)
+            {
+                *melhor_i = i;
+                *melhor_j = j;
+                fo_melhor_viz = fo_viz;
+            }
 
-      // desfaz o movimento
-      swap(s[i],s[j]);
+            // desfaz o movimento
+            swap(s[i], s[j]);
+        }
     }
-  }
-  // retornar a distancia do melhor vizinho
-  return fo_melhor_viz;
+    // retornar a distancia do melhor vizinho
+    return fo_melhor_viz;
 
-}//melhor_vizinho
+} // melhor_vizinho
 
-//Vizinho aleatorio usado nas descidas e algumas meta-heurisiticas
+// Vizinho aleatorio usado nas descidas e algumas meta-heurisiticas
 float vizinho_aleatorio(int n, vector<int> &s, float **d, float fo, int *melhor_i, int *melhor_j)
 {
     float fo_viz = fo;
     int i, j;
     float delta1, delta2;
 
-
     // retornar a distancia do  vizinho
     return fo_viz;
 
-}//vizinho_aleatorio 
+} // vizinho_aleatorio
 
 /* Método da descida com estrategia best improvement */
 float descida_best_improvement(int n, vector<int> &s, float **d)
 {
-  int melhor_i, melhor_j, iter;
-  float fo_viz, fo;
-  bool melhorou;
-  clock_t inicio_CPU, fim_CPU;
+    int melhor_i, melhor_j, iter;
+    float fo_viz, fo;
+    bool melhorou;
+    clock_t inicio_CPU, fim_CPU;
 
-  fo = fo_viz = calcula_fo(n, s, d);
-  limpa_arquivo((char*)"DescidaBI.txt");
-  inicio_CPU = fim_CPU = clock();
-  iter = 0;
-  imprime_fo((char*)"DescidaBI.txt", (fim_CPU - inicio_CPU)/CLOCKS_PER_SEC,fo,iter);
+    fo = fo_viz = calcula_fo(n, s, d);
+    limpa_arquivo((char *)"DescidaBI.txt");
+    inicio_CPU = fim_CPU = clock();
+    iter = 0;
+    imprime_fo((char *)"DescidaBI.txt", (fim_CPU - inicio_CPU) / CLOCKS_PER_SEC, fo, iter);
 
     /*  Implementar a descida com estrategia best improvement */
+    do {
+        melhorou = false;
+        fo_viz = melhor_vizinho(n, s, d, fo, &melhor_i, &melhor_j);
+        
+        if (fo_viz < fo) {
+            melhorou = true;
+            fo = fo_viz;
 
-
-
-  fim_CPU = clock();
-  imprime_fo((char*)"DescidaBI.txt", (fim_CPU - inicio_CPU)/CLOCKS_PER_SEC,fo,iter);
-  return fo;
-}//descida_best_improvement
+            swap(s[melhor_i], s[melhor_j]);
+        }
+    } while (melhorou);
+    
+    fim_CPU = clock();
+    imprime_fo((char *)"DescidaBI.txt", (fim_CPU - inicio_CPU) / CLOCKS_PER_SEC, fo, iter);
+    return fo;
+} // descida_best_improvement
 
 float descida_randomica(int n, vector<int> &s, float **d, int IterMax)
 {
-  int iter;
-  float fo, fo_viz;
+    int iter;
+    float fo, fo_viz;
 
-  int melhor_i, melhor_j;
-  clock_t inicio_CPU, fim_CPU;
+    int melhor_i, melhor_j;
+    clock_t inicio_CPU, fim_CPU;
 
-  fo = calcula_fo(n,s,d);
-  iter = 0;
-  limpa_arquivo((char*)"DescidaRandomica.txt");
-  inicio_CPU = fim_CPU = clock();
+    fo = calcula_fo(n, s, d);
+    iter = 0;
+    limpa_arquivo((char *)"DescidaRandomica.txt");
+    inicio_CPU = fim_CPU = clock();
 
-  while (iter < IterMax){
-    iter ++;
+    while (iter < IterMax)
+    {
+        iter++;
 
-  /*  Implementar a descida randômica  */
+        /*  Implementar a descida randômica  */
+    }
 
-
-  }
-
-  /* Implementar a impressão final (veja descida completa)*/
-
-  return fo;
+    /* Implementar a impressão final (veja descida completa)*/
+ 
+    return fo;
 }
-
-
 
 float vizinho_first_improvement(int n, vector<int> &s, float **d, float fo, int *melhor_i, int *melhor_j)
 {
- float fo_melhor_viz = fo;
- bool melhorou = false;
- vector<int> vet;
+    float fo_melhor_viz = fo;
+    bool melhorou = false;
+    vector<int> vet;
 
- for (int i=0; i < n; i++) vet.push_back(i);
- //Para c++ 11
- unsigned seed = chrono::system_clock::now().time_since_epoch().count();
- default_random_engine r(seed);
- shuffle ( vet.begin(), vet.end(), r );
- //Para c++ 98
- //random_shuffle ( vet.begin(), vet.end() );
+    for (int i = 0; i < n; i++)
+        vet.push_back(i);
+    // Para c++ 11
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine r(seed);
+    shuffle(vet.begin(), vet.end(), r);
+    // Para c++ 98
+    // random_shuffle ( vet.begin(), vet.end() );
 
- 
- // retornar a distancia do melhor vizinho
- return fo_melhor_viz;
+    // retornar a distancia do melhor vizinho
+    return fo_melhor_viz;
 
-}//melhor_vizinho
-
+} // melhor_vizinho
 
 float descida_first_improvement(int n, vector<int> &s, float **d)
 {
- int aux, melhor_i, melhor_j, iter;
- float fo_viz, fo;
- bool melhorou;
- clock_t inicio_CPU, fim_CPU;
+    int aux, melhor_i, melhor_j, iter;
+    float fo_viz, fo;
+    bool melhorou;
+    clock_t inicio_CPU, fim_CPU;
 
- fo = fo_viz = calcula_fo(n, s, d);
- limpa_arquivo((char*)"DescidaFI.txt");
- inicio_CPU = fim_CPU = clock();
- iter = 0;
- imprime_fo((char*)"DescidaFI.txt", (fim_CPU - inicio_CPU)/CLOCKS_PER_SEC,fo,iter);
+    fo = fo_viz = calcula_fo(n, s, d);
+    limpa_arquivo((char *)"DescidaFI.txt");
+    inicio_CPU = fim_CPU = clock();
+    iter = 0;
+    imprime_fo((char *)"DescidaFI.txt", (fim_CPU - inicio_CPU) / CLOCKS_PER_SEC, fo, iter);
 
-  /* Implementar a descida First Improvement armazenando, a cada iteração:
-      o tempo de execução
-      o valor da função de avaliação da solução corrente
-      a iteração na qual a solução vizinha foi aceita
-      Para fazer essa impressão, use o procedimento imprime_fo e imprime_fo_viz, 
-      disponíveis em Arquivos.cpp */
- 
+    /* Implementar a descida First Improvement armazenando, a cada iteração:
+        o tempo de execução
+        o valor da função de avaliação da solução corrente
+        a iteração na qual a solução vizinha foi aceita
+        Para fazer essa impressão, use o procedimento imprime_fo e imprime_fo_viz,
+        disponíveis em Arquivos.cpp */
 
-  fim_CPU = clock();
-  imprime_fo((char*)"DescidaFI.txt", (fim_CPU - inicio_CPU)/CLOCKS_PER_SEC,fo,iter);
-  return fo;
+    fim_CPU = clock();
+    imprime_fo((char *)"DescidaFI.txt", (fim_CPU - inicio_CPU) / CLOCKS_PER_SEC, fo, iter);
+    return fo;
 }
